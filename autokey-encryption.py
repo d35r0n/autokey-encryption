4868c745969d3432ad291b634864083fb85c5a5d
Ishaan Pathak <vaxtnhwm@anonaddy.me>
2019-07-19 00:12:30 +0530

    Update autokey-encryption.py

--- autokey-encryption.py
+++ autokey-encryption.py
@@ -1,88 +1,127 @@
 alph = [[]]
-alph.insert(0, ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
-alph.insert(1, ["B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A"])
-alph.insert(2, ["C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B"])
-alph.insert(3, ["D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C"])
-alph.insert(4, ["E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D"])
-alph.insert(5, ["F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E"])
-alph.insert(6, ["G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F"])
-alph.insert(7, ["H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G"])
-alph.insert(8, ["I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H"])
-alph.insert(9, ["J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I"])
-alph.insert(10, ["K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"])
-alph.insert(11, ["L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"])
-alph.insert(12, ["M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"])
-alph.insert(13, ["N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"])
-alph.insert(14, ["O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"])
-alph.insert(15, ["P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"])
-alph.insert(16, ["Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"])
-alph.insert(17, ["R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q"])
-alph.insert(18, ["S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R"])
-alph.insert(19, ["T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S"])
-alph.insert(20, ["U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"])
-alph.insert(21, ["V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U"])
-alph.insert(22, ["W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"])
-alph.insert(23, ["X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W"])
-alph.insert(24, ["Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X"])
-alph.insert(25, ["Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y"])
+alph.insert(0, ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+            "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
+alph.insert(1, ["B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A"])
+alph.insert(2, ["C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+            "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B"])
+alph.insert(3, ["D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C"])
+alph.insert(4, ["E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+            "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D"])
+alph.insert(5, ["F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+            "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E"])
+alph.insert(6, ["G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+            "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F"])
+alph.insert(7, ["H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+            "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G"])
+alph.insert(8, ["I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H"])
+alph.insert(9, ["J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+            "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I"])
+alph.insert(10, ["K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+            "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"])
+alph.insert(11, ["L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+            "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"])
+alph.insert(12, ["M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
+            "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"])
+alph.insert(13, ["N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y",
+            "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"])
+alph.insert(14, ["O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"])
+alph.insert(15, ["P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A",
+            "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"])
+alph.insert(16, ["Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B",
+            "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"])
+alph.insert(17, ["R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C",
+            "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q"])
+alph.insert(18, ["S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D",
+            "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R"])
+alph.insert(19, ["T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E",
+            "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S"])
+alph.insert(20, ["U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F",
+            "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"])
+alph.insert(21, ["V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G",
+            "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U"])
+alph.insert(22, ["W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H",
+            "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"])
+alph.insert(23, ["X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+            "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W"])
+alph.insert(24, ["Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X"])
+alph.insert(25, ["Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+            "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y"])
 
 result = []
 key1 = []
 
 calph = {
-    "A" : 1,
-    "B" : 2,
-    "C" : 3,
-    "D" : 4,
-    "E" : 5,
-    "F" : 6,
-    "G" : 7,
-    "H" : 8,
-    "I" : 9,
-    "J" : 10,
-    "K" : 11,
-    "L" : 12,
-    "M" : 13,
-    "N" : 14,
-    "O" : 15,
-    "P" : 16,
-    "Q" : 17,
-    "R" : 18,
-    "S" : 19,
-    "T" : 20,
-    "U" : 21,
-    "V" : 22,
-    "W" : 23,
-    "X" : 24,
-    "Y" : 25,
-    "Z" : 26}
-nums = {"0" : "9","1" : "8", "2" : "7", "3" : "6", "4" : "5", "5" : "4", "6" : "3", "7" : "2", "8" : "1", "9" : "0"}
+    "A": 0,
+    "B": 1,
+    "C": 2,
+    "D": 3,
+    "E": 4,
+    "F": 5,
+    "G": 6,
+    "H": 7,
+    "I": 8,
+    "J": 9,
+    "K": 10,
+    "L": 11,
+    "M": 12,
+    "N": 13,
+    "O": 14,
+    "P": 15,
+    "Q": 16,
+    "R": 17,
+    "S": 18,
+    "T": 19,
+    "U": 20,
+    "V": 21,
+    "W": 22,
+    "X": 23,
+    "Y": 24,
+    "Z": 25}
+nums = {"0": "9", "1": "8", "2": "7", "3": "6", "4": "5",
+    "5": "4", "6": "3", "7": "2", "8": "1", "9": "0"}
 
-#print(calph.get("A")-1)
 n = 0
 
-def encrypt(phrase,key,n):
+
+def encrypt(phrase, key, n):
 	res = ""
 	l = len(phrase)
-	while(l!=0):
+	while(l != 0):
 		key1.append(key[n])
 		if n == 2:
 			n = 0
 		else:
 			n = n + 1
 		l = l - 1
-	for i in range(0,len(phrase)):
+	for i in range(0, len(phrase)):
 		if(phrase[i].isalpha()):
 			if(phrase[i].isupper()):
-				result.append(alph[calph.get(key1[i])-1][calph.get(phrase[i])-1])
+				result.append(alph[calph.get(key1[i])][calph.get(phrase[i])])
 			else:
-				result.append(alph[calph.get(key1[i])-1][calph.get(phrase[i].upper())-1].lower())
+				result.append(alph[calph.get(key1[i])]
+				              [calph.get(phrase[i].upper())].lower())
 		elif(phrase[i].isnumeric()):
 			result.append(nums.get(phrase[i]))
 		else:
 			result.append(phrase[i])
 		res = res + (result[i])
 	print(res)
+
+def check(phrase,key,i):
+    if phrase[i].isalpha():
+        if phrase[i].isupper():
+            result.append(alph[0][alph[calph.get(key1[i])].index(phrase[i])])
+        else:
+            result.append(alph[0][alph[calph.get(key1[i])].index(phrase[i].upper())].lower())
+    elif phrase[i].isnumeric():
+        result.append(nums.get(phrase[i]))
+    else:
+        result.append(phrase[i])
 
 def decrypt(phrase,key,n):
 	res = ""
@@ -95,32 +134,20 @@ def decrypt(phrase,key,n):
 			n = n + 1
 		l = l - 1
 	for i in range(0,len(phrase)):
-		if(phrase[i].isalpha()):
-			if(phrase[i].isupper()):
-				ind = calph.get(key1[i])-1
-				g = alph[ind].index(phrase[i])
-				result.append(alph[0][g])
-			else:
-				phrase[i].upper()
-				ind = calph.get(key1[i])-1
-				g = alph[ind].index(phrase[i])
-				result.append(alph[0][g].lower())
-		elif(phrase[i].isnumeric()):
-			result.append(nums.get(phrase[i]))
-		else:
-			result.append(phrase[i])
+		check(phrase,key,i)
 	res = res + (result[i])
 	print(res)
 
-if __name__ == "__main__":
-    confirmation = input("What do you want to do?\n1) Encrypt\n2) Decrypt\n")
-    if confirmation == "1":
-        phrase  = input("Enter the Message to Encrypt: ")
-        key = input("Enter the Key to Encrypt: ")
-        encrypt(phrase,key,n)
-    elif confirmation == "2":
-        phrase  = input("Enter the Message to Decrypt: ")
-        key = input("Enter the Key to Decrypt: ")
-        decrypt(phrase, key,n)
-    else:
-        print("That's not a valid option!")
+# Main Code
+
+confirmation = input("What do you want to do?\n1) Encrypt\n2) Decrypt\n")
+if confirmation == "1":
+    phrase  = input("Enter the Message to Encrypt: ")
+    key = input("Enter the Key to Encrypt: ").upper()
+    encrypt(phrase,key,n)
+elif confirmation == "2":
+    phrase  = input("Enter the Message to Decrypt: ")
+    key = input("Enter the Key to Decrypt: ").upper()
+    decrypt(phrase, key,n)
+else:
+    print("That's not a valid option!")
